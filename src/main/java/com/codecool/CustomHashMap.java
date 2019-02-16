6710eecca01f403e227060718f79f50b9a774669
KrzysiekJodlowski <krzysztof.s.jodlowski@gmail.com>
2019-02-16 16:30:46 +0100

    Implement CustomHashMapTest and part of CustomHashMap classes

--- src/main/java/com/codecool/CustomHashMap.java
+++ src/main/java/com/codecool/CustomHashMap.java
@@ -1,5 +1,80 @@
 package com.codecool;
 
 
-public class CustomHashMap {
+import java.util.LinkedList;
+
+public class CustomHashMap<K, V> {
+
+    private int hashMapSize = 16;
+    private LinkedList<KeyValue>[] elements;
+
+    public CustomHashMap() {
+        this.elements = new LinkedList[this.hashMapSize];
+    }
+
+    public V add(K key, V value) throws CustomHashMapKeyException {
+
+        int position = getHash(key);
+        LinkedList<KeyValue> currentList = elements[position];
+
+        for (KeyValue keyValue : currentList) {
+            if (keyValue.getKey() == key) {
+                V oldValue = this.getValue(key);
+                keyValue.setValue(value);
+                return oldValue;
+            }
+        }
+        currentList.add(new KeyValue(key, value));
+        resizeIfNeeded();
+        return null;
+    }
+
+    public V getValue (K key) throws CustomHashMapKeyException {
+
+        int position = getHash(key);
+        LinkedList<KeyValue> currentList = elements[position];
+        V value = null;
+
+        for (KeyValue keyValue : currentList) {
+            if (keyValue.getKey() == key) {
+                value = (V) keyValue.getValue();
+            }
+        }
+        if (value == null) {
+            throw new CustomHashMapKeyException("key not found error");
+        }
+        return value;
+    }
+
+    public V remove(K key) {
+        return (V) "Value";
+    }
+
+    public boolean isEmpty() {
+        return false;
+    }
+
+    public void clearAll() {
+
+    }
+
+    public int size() {
+        return this.hashMapSize;
+    }
+
+    private int getHash (K key){
+        return key.hashCode() % this.hashMapSize;
+        // This function converts somehow the key to an integer between 0 and bucketSize
+        // In C# GetHashCode(), in Java hashCode() is a function of Object, so all non-primitive types
+        // can easily be converted to an integer.
+    }
+
+    private void resizeIfNeeded () {
+        // If it holds more elements than bucketSize * 2, destroy and recreate it
+        // with the double size of the elements array.
+        // if it holds less elements than bucketSize / 2, destroy and recreate it
+        // with half size of the elements array.
+    }
+
+    // + other functions, like clearAll(), delete(),..
 }
--- src/test/java/com/codecool/CustomHashMapTest.java
+++ src/test/java/com/codecool/CustomHashMapTest.java
@@ -1,9 +1,65 @@
 package com.codecool;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 
 class CustomHashMapTest {
+    private CustomHashMap customHashMap;
+
+    @BeforeEach
+    public void setCustomHashMap() {
+        this.customHashMap = new CustomHashMap();
+    }
+
+    @Test
+    public void testAddingToHashMap() throws CustomHashMapKeyException {
+
+        assertNull(this.customHashMap.add("Key", 27));
+        assertNull(this.customHashMap.add(28, "Value"));
+    }
+
+    @Test
+    public void testGettingAValue() throws CustomHashMapKeyException {
+
+        this.customHashMap.add("Key", 27);
+
+        assertEquals(27, this.customHashMap.getValue("Key"));
+    }
+
+    @Test
+    public void testRemovingData() throws CustomHashMapKeyException {
+
+        this.customHashMap.add("Key", 27);
+
+        assertEquals(27, this.customHashMap.remove("Key"));
+    }
+
+    @Test
+    public void testIsEmpty() {
+
+        assertTrue(this.customHashMap.isEmpty());
+    }
+
+    @Test
+    public void testClearingHashMap() throws CustomHashMapKeyException {
+
+        this.customHashMap.add("Key", 27);
+        this.customHashMap.add("Another key", 28);
+
+        this.customHashMap.clearAll();
+
+        assertTrue(this.customHashMap.isEmpty());
+    }
+
+    @Test
+    public void testGettingSizeOfAHashMap() throws CustomHashMapKeyException {
+
+        this.customHashMap.add("Key", 27);
+        this.customHashMap.add("Another key", 28);
+
+        assertEquals(2, this.customHashMap.size());
+    }
 
 }
